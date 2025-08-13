31d6456aa4bc1696cdec1cf82f2d2fee5f4be940
amarojumahesh <amarojumahesh238@gmail.com>
2025-08-13 08:40:32 +0000

    these is backend

---
+++ server/app.js
@@ -0,0 +1,12 @@
+console.log("Hello Backend")
+
+
+let fname="mahesh";
+let lname="shankar";
+console.log(fname + lname);
+
+
+//data types
+//1.primitive==> number ,string,boolean
+null,undefined
+//2.non primitive==>objects,and arrays
